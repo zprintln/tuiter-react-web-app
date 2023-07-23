9428f9b5e9fe27493f2044a977efc9bef642ccc1
zprintln <zadiemoon.println@gmail.com>
2023-07-23 10:39:55 -0700

    Resolving ESLint Errors

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link, useLocation } from "react-router-dom";
 const NavigationSidebar = () => {
  const { pathname } = useLocation();
- const [ignore, tuiter, active] = pathname.split("/");
+ const [, , active] = pathname.split("/");
  const links = ["home",     "explore",   "notifications", "messages", "bookmarks", "lists", "profile",  "more"];
  return (
    <div className="list-group">
